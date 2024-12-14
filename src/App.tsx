abe67c22e81d48e9b6f54951b2a3160a2d816da5
Delziron Lima <delziron.braz@hotmail.copm>
2024-12-14 12:51:29 -0300

    feat(create phones table)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { PhoneService } from "./services/phone.service";
+import { IPhone } from "./interfaces/phone.interface";
+import { MockPhoneService } from "./__mock__/phones/phones.mock";
+import DataTable from "./components/DataTable/DataTable";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [phones, setPhones] = useState<IPhone[]>([]);
+
+  useEffect(() => {
+    if (import.meta.env.VITE_NODE_ENV === "test") {
+      const mockPhones = MockPhoneService.getPhones();
+      setPhones(mockPhones);
+    } else {
+      PhoneService.getPhones().then((phones) => setPhones(phones));
+    }
+  }, []);
+
+  return (
+    <>
+      <ToastContainer />
+      <main className="w-screen h-full flex flex-col justify-start items-center mx-auto p-8">
+        <DataTable phones={phones} setPhones={setPhones} />
+      </main>
+    </>
+  );
 }
---
+++ src/components/DataTable/DataTable.tsx
@@ -0,0 +1,121 @@
+import { Button, IconButton } from "@mui/material";
+import { MdSmartphone } from "react-icons/md";
+import { FaPlus } from "react-icons/fa6";
+import { IPhone } from "../../interfaces/phone.interface";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { MdDelete } from "react-icons/md";
+import { HiPencil } from "react-icons/hi2";
+import { MockPhoneService } from "../../__mock__/phones/phones.mock";
+import { PhoneService } from "../../services/phone.service";
+import { toast } from "react-toastify";
+
+interface DataTableProps {
+  phones: IPhone[];
+  setPhones?: (phones: IPhone[]) => void;
+}
+
+const DataTable = ({ phones, setPhones }: DataTableProps) => {
+  const handleDelete = (code: string) => {
+    if (import.meta.env.VITE_NODE_ENV === "test") {
+      const result = MockPhoneService.deletePhone(code);
+      if (result.success) {
+        toast.success("Registro excluído com sucesso", {
+          position: "top-right",
+          autoClose: 3000,
+        });
+        if (result.phones) {
+          setPhones?.(result.phones);
+        }
+      }
+    } else {
+      PhoneService.deletePhone(code)
+        .then(() => {
+          PhoneService.getPhones().then((phones) => setPhones?.(phones));
+        })
+        .catch((error) => {
+          console.error(error);
+          toast.error("Erro ao excluir registro", {
+            position: "top-right",
+            autoClose: 3000,
+          });
+        });
+      toast.success("Registro excluído com sucesso", {
+        position: "top-right",
+        autoClose: 3000,
+      });
+    }
+  };
+
+  return (
+    <section className="w-full h-full mx-auto flex justify-center items-center">
+      <div className="max-w-[1250px] w-full flex flex-col justify-center items-center gap-4">
+        <div className="w-full flex flex-col md:flex-row justify-between items-center gap-5">
+          <h4 className="font-bold text-2xl text-black">Produtos</h4>
+          <Button
+            variant="contained"
+            startIcon={
+              <div className="flex justify-start items-center">
+                <FaPlus />
+                <MdSmartphone />
+              </div>
+            }
+          >
+            Adicionar
+          </Button>
+        </div>
+
+        <div className="w-full">
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Código</TableCell>
+                  <TableCell align="right">Modelo</TableCell>
+                  <TableCell align="right">Preço</TableCell>
+                  <TableCell align="right">Marca</TableCell>
+                  <TableCell align="right">Cor</TableCell>
+                  <TableCell align="right"></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {phones.map((row, index) => (
+                  <TableRow
+                    key={index}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.code}
+                    </TableCell>
+                    <TableCell align="right">{row.model}</TableCell>
+                    <TableCell align="right">{row.price}</TableCell>
+                    <TableCell align="right">{row.brand}</TableCell>
+                    <TableCell align="right">{row.color}</TableCell>
+                    <TableCell
+                      align="right"
+                      className="flex justify-center items-center"
+                    >
+                      <IconButton onClick={() => handleDelete(row.code)}>
+                        <MdDelete />
+                      </IconButton>
+                      <IconButton>
+                        <HiPencil />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default DataTable;
