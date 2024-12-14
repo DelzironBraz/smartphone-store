6864b2c5d09ecbbe86fd109514b3a351461171ac
Delziron Lima <delziron.braz@hotmail.copm>
2024-12-14 15:37:02 -0300

    chore(create new routes)

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Header from "./components/Header/Header.tsx";
 import Footer from "./components/Footer/Footer.tsx";
+import CreatePhone from "./pages/create/page.tsx";
+import EditPhone from "./pages/edit/page.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -12,6 +14,10 @@ createRoot(document.getElementById("root")!).render(
       <Header />
       <Routes>
         <Route index element={<App />} />
+        <Route path="phone">
+          <Route path="create" element={<CreatePhone />} />
+          <Route path=":code" element={<EditPhone />} />
+        </Route>
       </Routes>
       <Footer />
     </BrowserRouter>
