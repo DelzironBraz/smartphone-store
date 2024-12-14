c97206932cf744c13fd1332003261e45aee9be55
Delziron Lima <delziron.braz@hotmail.copm>
2024-12-14 12:50:42 -0300

    feat(create footer)

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="w-screen bg-primary flex justify-center items-center gap-1 py-10">
+      <p className="text-white text-center text-md">
+        © Copyright 2020 - Melhor Celular - Todos os direitos reservados à
+        Melhor Celular LTDA.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router";
+import Header from "./components/Header/Header.tsx";
+import Footer from "./components/Footer/Footer.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route index element={<App />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   </StrictMode>
 );
