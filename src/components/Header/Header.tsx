ba1f881029473e19366b8daa456cd57268968bb4
Delziron Lima <delziron.braz@hotmail.copm>
2024-12-14 15:36:26 -0300

    styles(add link for home to header)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import { IoPhonePortraitSharp } from "react-icons/io5";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="w-screen bg-primary flex justify-center items-center gap-1 py-5">
-      <span className="font-bold text-white text-5xl">M</span>
-      <IoPhonePortraitSharp size={34} color="white" />
+      <NavLink to="/" className="flex justify-center items-center gap-1">
+        <span className="font-bold text-white text-5xl">M</span>
+        <IoPhonePortraitSharp size={34} color="white" />
+      </NavLink>
     </header>
   );
 };
